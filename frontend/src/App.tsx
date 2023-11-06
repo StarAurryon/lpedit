97614548c72f139194fb9853c6aa746e420c9e2e
StarAurryon <9308314+StarAurryon@users.noreply.github.com>
2023-11-06 20:40:29 +0100

    feat: adding tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     return (
         <div id="App">
             <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
+            <div id="result" className="text-3xl font-bold underline">{resultText}</div>
             <div id="input" className="input-box">
                 <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
                 <button className="btn" onClick={greet}>Greet</button>
